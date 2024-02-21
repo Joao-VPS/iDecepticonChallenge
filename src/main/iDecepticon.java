7081cf633416c798ee5933cc5ea4e7ddeffc786e
Joao-VPS <joaovictorpuglessabino@gmail.com>
2024-02-21 11:51:26 -0300

    Creating a backup after adding contributors.
    Co-authored-by: Gabriel Rodrigues <gabers357@gmail.com>
    Co-authored-by: GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
    Co-authored-by: bruno4224onurb <144866243+bruno4224onurb@users.noreply.github.com>
    Co-authored-by: Douglas Lima <douglas.lima.queiroz@gmail.com>
    Co-authored-by: Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
    Co-authored-by: Larissa Sanches <larissaesanches@gmail.com>

--- src/main/iDecepticon.java
+++ src/main/iDecepticon.java
@@ -1,10 +1,133 @@
 package main;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class iDecepticon {
+	boolean affirmation, question, hasGreeting;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		//Quebrou totalmente a lógica :p
+		String pseudoInput = "Hello! My name is John!";
+		String[] phraseWords = pseudoInput.split(" ");
+		String[] phraseLength = new String[phraseWords.length];
+		
+		for (int i = 0; i < phraseLength.length; i++) {
+			if (greetings(phraseWords[i])) phraseLength[i] = "greeting";
+		}
+		
+		if (greetings(phraseWords[0])) {
+			hasGreeting = true;
+		}
 	}
 
+	// Função de semântica
+	public String semantic() {
+		
+	}
+	
+	public String semanticAffirmation() {
+		
+	}
+	
+	// Funções verificadoras de palavras
+	public static boolean greetings(String input) {
+		String[] greetings = {
+			"hi", "hello", "hey",
+			"hey man", "hey bro", "fellas",
+			"what's up", "good morning",
+			"good afternoon", "good evening"
+		};
+		
+		for (String word : greetings) {
+			if (input.equalsIgnoreCase(word)) return true;
+		}
+		
+		return false;
+	}
+	
+	public static String gender(String input) {
+		String[] male = {
+			"he", "him", "his", "gabriel", "douglas", "joão", "john", "bruno", "leandro"
+		};
+		
+		String[] female = {
+			"she", "her", "hers", "larissa", "camilla"
+		};
+		
+		String[] neutral = {
+			"it", "we", "they", "them", "theirs"
+		};
+		
+		for (String i: male) {
+			if (input.equalsIgnoreCase(i)) return "male";
+		}
+		
+		for (String i: female) {
+			if (input.equalsIgnoreCase(i)) return "female";
+		}
+		
+		for (String i: neutral) {
+			if (input.equalsIgnoreCase(i)) return "neutral";
+		}
+		
+		return "unknown";
+	}
+
+	public static boolean person(String input) {
+		String[] pronouns = {
+				"i", "you", "we",
+				"it", "they", "he",
+				"she"
+		};
+		
+		for (String i: pronouns) {
+			if (input.equalsIgnoreCase(i)) return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean auxiliaries(String input) {
+		String[] verbs = {
+			"do", "does", "did", "be"
+		};
+		
+		for (String i: verbs) {
+			if (input.equalsIgnoreCase(i)) return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean possessive(String input) {
+		String[] possessive = {
+			"my", "your", "yours", "our", "ours", "mine"};
+		
+		for (String i: possessive) {
+			if (input.equalsIgnoreCase(i)) return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean questionWords(String input) {
+		String[] questionWords = {
+				"who", "whom", "whose",
+				"when", "why", "which",
+				"how", "have", "what",
+				"has", "had", "will",
+				"would", "can", "could",
+				"should", "might", "where"};
+		
+		for (String i: questionWords) {
+			if (input.equalsIgnoreCase(i)) return true;
+		}
+		
+		return false;
+	}
 }
